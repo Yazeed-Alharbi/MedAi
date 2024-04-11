f06c0d18583118fb60331ff579a68be3018c838b
Yazeed-Alharbi <94597714+Yazeed-Alharbi@users.noreply.github.com>
2024-04-11 18:20:32 +0300

    Refine styling for email, password, and text input fields

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
     let colorModeIcon = document.getElementById("color-mode-icon");
     let loginBackgroundImage = document.querySelector(".col.image-overlay img:nth-of-type(1)");
     let loginBackgroundLogo = document.querySelector(".col.image-overlay img:nth-of-type(2)");
+    let showPasswordToggle = document.getElementById("show-password-toggle");
+    const passwordInput = document.getElementById('password');
     
     colorModeIcon.onclick = function(){
         document.body.classList.toggle("dark-mode");
@@ -16,4 +18,18 @@ document.addEventListener("DOMContentLoaded", function(){
             loginBackgroundLogo.src ="assets/MedAi_logo_dark.png";
         }
     }
+
+    showPasswordToggle.addEventListener('click', function() {
+        const isPasswordVisible = passwordInput.getAttribute('type') === 'text';
+        passwordInput.setAttribute('type', isPasswordVisible ? 'password' : 'text');
+
+        if (isPasswordVisible) {
+            showPasswordToggle.classList.remove('fa-eye-slash');
+            showPasswordToggle.classList.add('fa-eye');
+        } else {
+            showPasswordToggle.classList.remove('fa-eye');
+            showPasswordToggle.classList.add('fa-eye-slash');
+        }
+});
+
 });
