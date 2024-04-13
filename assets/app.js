7461aebdce836ef3ce3ca7e9cec1e1f33e7f138c
Yazeed-Alharbi <94597714+Yazeed-Alharbi@users.noreply.github.com>
2024-04-14 01:03:52 +0300

    Rebuild and enhance login page

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     let colorModeIcon = document.getElementById("color-mode-icon");
-    let loginBackgroundImage = document.querySelector(".col.image-overlay img:nth-of-type(1)");
-    let loginBackgroundLogo = document.querySelector(".col.image-overlay img:nth-of-type(2)");
+    let loginBackgroundLogo = document.getElementById("logo");
     let showPasswordToggle = document.getElementById("show-password-toggle");
     const passwordInput = document.getElementById('password');
     
@@ -9,12 +8,10 @@ document.addEventListener("DOMContentLoaded", function(){
         document.body.classList.toggle("dark-mode");
         if (document.body.classList.contains("dark-mode")){
             colorModeIcon.src = "assets/lightmode_icon.png";
-            loginBackgroundImage.src = "assets/background_image_dark.png";
             loginBackgroundLogo.src ="assets/MedAi_logo_light.png";
         }
         else{
             colorModeIcon.src = "assets/darkmode_icon.png";  
-            loginBackgroundImage.src = "assets/background_image_light.png";
             loginBackgroundLogo.src ="assets/MedAi_logo_dark.png";
         }
     }
