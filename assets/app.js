268515729700a72b50e67a054416b2271836c065
Yazeed-Alharbi <94597714+Yazeed-Alharbi@users.noreply.github.com>
2024-04-15 22:14:54 +0300

    Enable dark mode across all website

--- assets/app.js
+++ assets/app.js
@@ -4,17 +4,43 @@ document.addEventListener("DOMContentLoaded", function(){
     let showPasswordToggle = document.getElementById("show-password-toggle");
     const passwordInput = document.getElementById('password');
     
-    colorModeIcon.onclick = function(){
-        document.body.classList.toggle("dark-mode");
-        if (document.body.classList.contains("dark-mode")){
-            colorModeIcon.src = "assets/lightmode_icon.png";
-            loginBackgroundLogo.src ="assets/MedAi_logo_light.png";
+    function darkModeSetter(){
+        let isDarkMode = localStorage.getItem("darkMode") === "true";
+        if (isDarkMode){
+
+            document.body.classList.toggle("dark-mode", isDarkMode);
+            if (window.location.pathname.includes("/login.html")){
+                colorModeIcon.src = "assets/lightmode_icon.png";
+                loginBackgroundLogo.src ="assets/MedAi_logo_light.png";
+            }
         }
         else{
-            colorModeIcon.src = "assets/darkmode_icon.png";  
-            loginBackgroundLogo.src ="assets/MedAi_logo_dark.png";
+            if (window.location.pathname.includes("/login.html")){
+                
+                colorModeIcon.src = "assets/darkmode_icon.png";  
+                loginBackgroundLogo.src ="assets/MedAi_logo_dark.png";
+            }
+            document.body.classList.toggle("dark-mode", isDarkMode);
         }
     }
+    darkModeSetter();
+    if (colorModeIcon){
+    colorModeIcon.onclick = function(){
+        let isDarkMode = localStorage.getItem("darkMode") === "true";
+
+        if(isDarkMode){
+            localStorage.setItem("darkMode", "false");
+
+            
+        }
+        else{
+            localStorage.setItem("darkMode", "true");
+            
+            
+        }
+        darkModeSetter();
+    }
+}
 
     showPasswordToggle.addEventListener('click', function() {
         const isPasswordVisible = passwordInput.getAttribute('type') === 'text';
