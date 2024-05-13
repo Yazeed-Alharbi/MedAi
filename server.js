0c5cbed649fb6d676040cf163c4cfae3fca33f34
Waleed <94017717+Waleed-Almutairi@users.noreply.github.com>
2024-05-13 15:14:00 +0300

    complete routes

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const nunjucks = require("nunjucks");
+const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const PORT = 3000;
+
+app.get("/", async function (req, res) {
+  res.render("login.njk");
+});
+
+// #TODO: Add authentication
+app.get("/dashboard", async function (req, res) {
+  res.render("dashboard.njk");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
