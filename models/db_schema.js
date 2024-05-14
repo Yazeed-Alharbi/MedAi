4e7f0837e8e1ad056f9f5f2b19e2a116f5d0b7cd
Yazeed-Alharbi <94597714+Yazeed-Alharbi@users.noreply.github.com>
2024-05-14 17:25:07 +0300

    Fix dashboard template and db schema

--- models/db_schema.js
+++ models/db_schema.js
@@ -3,49 +3,82 @@ const mongoose = require("mongoose");
 const patientSchema = new mongoose.Schema({
   patient_id: {
     type: Number,
-    required: true,
+    required: true
   },
   name: {
     type: String,
-    required: true,
+    required: true
   },
   gender: {
     type: String,
-    required: true,
+    enum: ['M', 'F', 'Other'],
+    required: true
   },
   medical_conditions: {
     type: [String],
-    default: [],
+    required: true
   },
   medications: {
-    type: Array,
-    default: [],
+    type: [String],
+    required: true
   },
   allergies: {
-    type: Array,
-    default: [],
-  },
-  last_appointment_date: {
-    type: String,
+    type: [String],
+    required: true
   },
   blood_type: {
     type: String,
+    required: true
   },
   BMI: {
     type: Number,
+    required: true
   },
   age: {
     type: Number,
+    required: true
   },
   Current_Disease: {
     type: String,
+    default: 'None'
   },
   Mental_Health_Condition: {
-    type: String,
+    type: [String]
   },
   Current_Diagnosis: {
-    type: String,
+    type: String
   },
+  last_appointment: {
+    date: {
+      type: String,
+      required: true
+    },
+    institution: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    }
+  },
+  place_of_birth: {
+    type: String,
+    required: true
+  },
+  past_surgeries: {
+    type: [String]
+  },
+  lab_tests: {
+    type: [String]
+  }
 });
 
 const physicianSchema = new mongoose.Schema({
